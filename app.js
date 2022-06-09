c90706179edc2e244ef1892a37b895ca3bb01a9e
tuantruong <tuantruong.a8k29@gmail.com>
2022-06-10 02:27:06 +0700

    fix url , csp6

--- app.js
+++ app.js
@@ -27,14 +27,6 @@ function getcookie(req) {
 dotenv.config({ path: "./config.env" });
 const app = express();
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Content-Security-Policy-Report-Only",
-    "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic';  script-src * data: blob: 'unsafe-inline' 'unsafe-eval';  connect-src * data: blob: 'unsafe-inline';  img-src * data: blob: 'unsafe-inline';  frame-src * data: blob: ;  style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob: 'unsafe-inline'; "
-  );
-  next();
-});
-
 // app.use(
 //   cors({ credentials: true, origin: "https://tn-travelxd.herokuapp.com" })
 // );
@@ -63,6 +55,13 @@ app.use("*", (req, res, next) => {
   }
 
   // console.log(cookie);
+  next();
+});
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy-Report-Only",
+    "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic';  script-src * data: blob: 'unsafe-inline' 'unsafe-eval';  connect-src * data: blob: 'unsafe-inline';  img-src * data: blob: 'unsafe-inline';  frame-src * data: blob: ;  style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob: 'unsafe-inline'; "
+  );
   next();
 });
 app.use(hpp({ whitelist: ["duration"] }));
