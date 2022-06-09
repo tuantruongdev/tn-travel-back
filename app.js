5136a224dbca3cc5ff983656ab6f335e73703af7
tuantruong <tuantruong.a8k29@gmail.com>
2022-06-10 02:19:41 +0700

    fix url , csp6

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.setHeader(
-    "Content-Security-Policy",
+    "Content-Security-Policy-Report-Only",
     "default-src *  data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic';  script-src * data: blob: 'unsafe-inline' 'unsafe-eval';  connect-src * data: blob: 'unsafe-inline';  img-src * data: blob: 'unsafe-inline';  frame-src * data: blob: ;  style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob: 'unsafe-inline'; "
   );
   next();
